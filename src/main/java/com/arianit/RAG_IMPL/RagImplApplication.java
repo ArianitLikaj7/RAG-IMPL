b3a34078e5dca21fa95277b97b1ca2d84f00d2b3
Arianit Likaj <arianitlikaj46@gmail.com>
2025-07-06 23:49:37 +0200

    fix

--- src/main/java/com/arianit/RAG_IMPL/RagImplApplication.java
+++ src/main/java/com/arianit/RAG_IMPL/RagImplApplication.java
@@ -1,8 +1,5 @@
 package com.arianit.RAG_IMPL;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -13,15 +10,4 @@ public class RagImplApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RagImplApplication.class, args);
 	}
-
-	@Bean
-	public ChatClient openAiChatClient(OpenAiChatModel model) {
-		return ChatClient.create(model);
-	}
-
-	@Bean
-	public ChatClient ollamaChatModel(OllamaChatModel model) {
-		return ChatClient.create(model);
-	}
-
 }
